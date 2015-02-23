a8d2a3662d0a84ef9b4f1f4ef3ce991f7674f6fd
wspl <widesense.plqws@gmail.com>
2015-02-23 22:31:08 +0800

    纠正缩进

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 var config = require('./config');
 var express = require('express'),
-		app = express();
+    app = express();
 
 app.get('/', function(req, res){
-	res.sendFile(__dirname + '/index.html')
+  res.sendFile(__dirname + '/index.html')
 });
 
 app.get('/api/:command', function(req, res){
@@ -15,5 +15,5 @@ app.get('/api/submit/:command', function(req, res){
 });
 
 app.listen(config.listen.port, config.listen.host, function(){
-	console.log('ACGMedia-Server listening at ' + config.listen.host + ':' + config.listen.port)
+  console.log('ACGMedia-Server listening at ' + config.listen.host + ':' + config.listen.port)
 });
--- config.js
+++ config.js
@@ -1,15 +1,15 @@
 module.exports = {
-	listen: {
-		host: '127.0.0.1',
-		port: 3000
-	},
-	db: {
-		user: 'acgmedia',
-		pass: 'acgmedia_rAg4!a',
-		name: 'acgmedia',
-		details: { //sequelize options
-			host: 'localhost',
-			dialect: 'mariadb'
-		}
-	}
+  listen: {
+    host: '127.0.0.1',
+    port: 3000
+  },
+  db: {
+    user: 'acgmedia',
+    pass: 'acgmedia_rAg4!a',
+    name: 'acgmedia',
+    details: { //sequelize options
+      host: 'localhost',
+      dialect: 'mariadb'
+    }
+  }
 }
